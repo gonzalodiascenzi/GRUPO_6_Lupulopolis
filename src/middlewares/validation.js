0791570ba3ced71b530c18b7c29611df4a1637ef
MatiasBossa <matubossa@gmail.com>
2021-02-09 11:27:40 -0300

    Se realizaron cambios en el data.sql, y validaciones

--- src/middlewares/validation.js
+++ src/middlewares/validation.js
@@ -7,15 +7,15 @@ module.exports = {
     loginValidation: [
         body('email')
             .notEmpty().withMessage("El campo es obligatorio")
-                .bail()
+            .bail()
             .isEmail().withMessage("El email ingresado no es valido")
-                .bail()
-            .custom( async (emailValue, { req }) => {
+            .bail()
+            .custom(async (emailValue, { req }) => {
                 const userFound = await db.User.findOne({ where: { email: emailValue } });
 
                 if (!userFound || !bcryptjs.compareSync(req.body.password, userFound.password)) {
                     return Promise.reject('El usuario o contraseña no coiciden');
-                } 
+                }
 
             }),
         body('password')
@@ -25,27 +25,27 @@ module.exports = {
     registerValidation: [
         body('first_name')
             .notEmpty().withMessage("El campo obligatorio")
-                .bail()
-            .isLength({ min : 2 }).withMessage("Ingrese un minimo de 2 caracteres"),
+            .bail()
+            .isLength({ min: 2 }).withMessage("Ingrese un minimo de 2 caracteres"),
         body('last_name')
             .notEmpty().withMessage("El campo obligatorio")
-                .bail()
+            .bail()
             .isLength({ min: 2 }).withMessage("Ingrese un minimo de 2 caracteres"),
         body('image')
             .notEmpty().withMessage('El campo es obligatorio 3')
-                .bail()
+            .bail()
             .custom((value, { req }) => {
                 const validExtends = ['.jpg', '.jpeg', '.png', '.gif'];
                 console.log(req.file);
                 const fileExtend = path.extname(req.file.originalname);
 
                 return validExtends.includes(fileExtend);
-            }), 
+            }),
         body('email')
             .notEmpty().withMessage("El campo obligatorio")
-                .bail()
+            .bail()
             .isEmail().withMessage("El email ingresado no es valido")
-                .bail()
+            .bail()
             .custom(async emailValue => {
                 const emailExist = await db.User.findOne({ where: { email: emailValue } });
 
@@ -57,32 +57,33 @@ module.exports = {
             }),
         body('password')
             .notEmpty().withMessage("El campo obligatorio")
-                .bail()
+            .bail()
             .isLength({ min: 8 }).withMessage("Ingrese un minimo de 8 caracteres")
-                .bail()
-            .custom((passwordValue, { req }) => {
-
-                return passwordValue == req.body.retype && passwordValue.match(/[a-z]/) && passwordValue.match(/[A-Z]/) && passwordValue.match(/\d/g) &&  passwordValue.match(/^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/)             
-            }).withMessage("El password no contiene los caracteres minimos solicitados."),
+            .bail()
+        // .custom((passwordValue, { req }) => {
+        // En desarrollo
+        //     return passwordValue == req.body.retype && passwordValue.match(/[a-z]/) && passwordValue.match(/[A-Z]/) && passwordValue.match(/\d/g) &&  passwordValue.match(/^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/)             
+        // }).withMessage("El password no contiene los caracteres minimos solicitados.")
+        ,
         body('retype')
             .notEmpty().withMessage("Complete el campo repetir password")
     ],
     updateProductValidation: [
         body('product_name')
             .notEmpty().withMessage("El campo es obligatorio")
-                .bail()
+            .bail()
             .isLength({ min: 5 }).withMessage("El campo debe tener al menos 5 caracteres"),
         body('description')
             .notEmpty().withMessage("El campo es obligatorio")
-                .bail()
+            .bail()
             .isLength({ min: 20 }).withMessage("El campo debe tener al menos 20 caracteres"),
         body('image')
-            .custom(( imageFile, { req } )=> {
+            .custom((imageFile, { req }) => {
                 const extendsValid = [".jpg", ".jpeg", ".png", ".gif"];
-                const fileExtend = path.extname(req.files[0].originalame);
+                const fileExtend = path.extname(req.file.originalame);
 
                 return extendsValid.includes(fileExtend);
-            })
+            }).withMessage(`La imagen debe contener los formatos permitidos : ${extendsValid}`)
     ],
     createProductValidation: [
         body('product_name')
@@ -96,9 +97,9 @@ module.exports = {
         body('image')
             .custom(( imageFile, { req } )=> {
                 const extendsValid = [".jpg", ".jpeg", ".png", ".gif"];
-                const fileExtend = path.extname(req.files[0].originalame);
+                const fileExtend = path.extname(req.file.originalame);
 
                 return extendsValid.includes(fileExtend);
-            })
+            }).withMessage(`La imagen debe contener los formatos permitidos : ${extendsValid}`)
     ]
 }
